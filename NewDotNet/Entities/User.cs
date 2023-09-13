8aa8c4afe5e6aed325ce169640d28775201be0b1
tMirjalol <toraboyevmirjalol2923@gmail.com>
2023-09-13 11:38:53 +0500

    Create UserId

--- NewDotNet/Entities/User.cs
+++ NewDotNet/Entities/User.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NewDotNet.Entities
+﻿namespace NewDotNet.Entities;
+public class User
 {
-    internal class User
-    {
-    }
+    public long Id { get; set; }
 }
