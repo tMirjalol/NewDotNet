057913ad490088e4f96ee5976302302af8172a5e
tMirjalol <toraboyevmirjalol2923@gmail.com>
2023-09-14 07:59:30 +0500

    User and Product added

---
+++ NewDotNet/Entities/Product.cs
@@ -0,0 +1,7 @@
+﻿
+namespace NewDotNet.Entities;
+
+public class Product
+{
+    public string Name { get; set; }
+}
--- NewDotNet/Entities/User.cs
+++ NewDotNet/Entities/User.cs
@@ -2,4 +2,6 @@
 public class User
 {
     public long Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
 }
